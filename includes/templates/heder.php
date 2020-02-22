93e29e823e90fd1a42eda403c972f22d1927e6df
islammohamed123 <islammohamed323@gmail.com>
2020-02-22 21:21:06 +0200

    fix login again

--- includes/templates/heder.php
+++ includes/templates/heder.php
@@ -5,11 +5,12 @@
 	<title> <?php echo 'pageName'; ?> </title>
 
 	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-
+<!-- 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+ -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!--file bootstrab in localhost -->
+	<link rel="stylesheet" href="<?php echo $css; ?>bootstrap.css">
 	<link rel="stylesheet" href="<?php echo $css; ?>bootstrap.min.css">
 	<!-- font_awsom -->
 	<link rel="stylesheet" href="<?php echo $css; ?>all.min.css"> 
