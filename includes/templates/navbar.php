7b9794863b7a5eb60b249b431890e09313200c06
islammohamed123 <islammohamed323@gmail.com>
2020-02-22 18:29:09 +0200

    fix navbar and unrated push

--- includes/templates/navbar.php
+++ includes/templates/navbar.php
@@ -8,7 +8,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto navbar-right">
       <li class="nav-item">
-        <a class="nav-link" href="#"><img class="circle-img" src="layout/imgs/social.jpg"> <span>User Name</span></a>
+        <a class="nav-link" href="#"><img class="circle-img" src="layout/imgs/killue.jpg"> <span>User Name</span></a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
