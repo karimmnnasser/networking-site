f10013a7e230f79271892f41ed7b81178760717d
islammohamed123 <islammohamed323@gmail.com>
2020-02-22 17:46:54 +0200

    Navbar and some play

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -10,7 +10,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
 <!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 	<script type="<?php echo $js; ?>script.js"></script>
 </body>
--- includes/templates/navbar.php
+++ includes/templates/navbar.php
@@ -1,13 +1,24 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark nav-z">
-  <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary cont">
+ <div class="container">
+    <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <ul class="navbar-nav mr-auto navbar-right">
+      <li class="nav-item">
+        <a class="nav-link" href="#"><img class="circle-img" src="layout/imgs/social.jpg"> <span>User Name</span></a>
+      </li>
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <div class="vl"></div>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
@@ -24,6 +35,6 @@
         </div>
       </li>
     </ul>
-
   </div>
+ </div>
 </nav>
