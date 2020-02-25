887e8e9f5a299336c68b9f08490fe46af1789feb
karim <karimnasser01032847726@gmail.com>
2020-02-25 09:13:30 +0200

    fixed

---
+++ checkEmail.php
@@ -0,0 +1,47 @@
+<?php
+include "init.php";
+
+
+
+
+
+?>
+
+
+
+<div class="container">
+    <form class="col-3 checkEmail" action="">
+        <div class="form-group">
+            <label>Please write it down.</label>
+            <input type="email" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include $temp . "footer.php"; ?>
