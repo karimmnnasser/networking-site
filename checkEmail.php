69e1b6cd33b1de9ce6c611bb308a425fbec3ecec
karim <karimnasser01032847726@gmail.com>
2020-02-25 11:34:17 +0200

    test

--- checkEmail.php
+++ checkEmail.php
@@ -10,14 +10,23 @@ include "init.php";
 
 
 <div class="container">
-    <form class="col-3 checkEmail" action="">
+    <form class="col-6 checkEmail" action="" method="">
         <div class="form-group">
-            <label>Please write it down.</label>
-            <input type="email" class="form-control">
+            <label>Please check your E-mail and write nubmer code in down.</label>
+            <input type="text" class="form-control">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary btn-block btn-checkEmail">Submit</button>
     </form>
 </div>
+
+<div class="checkEmailImg">
+            <div class="imgs">
+                <img src="layout/imgs/ms.svg">
+            </div>
+    
+            <div class="text">
+            </div>
+    </div>
 
 
 
