efda6253437a73a1759dee67059907fde28e2c66
karim <karimnasser01032847726@gmail.com>
2020-03-14 16:27:08 +0200

    add database

--- database/test.sql
+++ database/test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2020 at 03:01 PM
+-- Generation Time: Mar 14, 2020 at 03:25 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -25,6 +25,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `temporary_code`
+--
+
+CREATE TABLE `temporary_code` (
+  `id` int(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Temporary_code` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `temporary_code`
+--
+
+INSERT INTO `temporary_code` (`id`, `Email`, `Temporary_code`) VALUES
+(42, 'karimnasser01032847726@gmail.com', 40410);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -35,6 +54,7 @@ CREATE TABLE `users` (
   `email` varchar(255) CHARACTER SET utf8 NOT NULL,
   `password` varchar(255) CHARACTER SET utf8 NOT NULL,
   `re_password` int(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -42,12 +62,18 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `first_name`, `last_name`, `email`, `password`, `re_password`, `country`) VALUES
-(1, 'karim', 'nasser', 'karim@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 0, '');
+INSERT INTO `users` (`userID`, `first_name`, `last_name`, `email`, `password`, `re_password`, `gender`, `country`) VALUES
+(24, 'karim', 'nasser', 'karimnasser01032847726@gmail.com', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2', 6216, 'male', '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `temporary_code`
+--
+ALTER TABLE `temporary_code`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +87,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `temporary_code`
+--
+ALTER TABLE `temporary_code`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
